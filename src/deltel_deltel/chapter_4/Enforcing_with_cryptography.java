9220267debdfe8f2d5ba6b7f83e203bebc4aa54a
FavourPreciousOchonogor(Precy2020) <f.ochonogor@native.semicolon.africa>
2023-07-22 20:55:00 +0100

    add

---
+++ src/deltel_deltel/chapter_4/Enforcing_with_cryptography.java
@@ -0,0 +1,35 @@
+package deltel_deltel.chapter_4;
+
+import java.util.Scanner;
+
+public class Enforcing_with_cryptography {
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        System.out.println(">> ");
+        int number = key.nextInt();
+
+        int a = number / 1000;
+        int b = number % 1000 / 100;
+        int c = number % 1000 % 100 / 10;
+        int d = number % 1000 % 100 % 10 / 1;
+
+        int first = a + 7;
+        int second = b + 7;
+        int third = c + 7;
+        int fourth = d + 7;
+        System.out.println(first + " " + second + " " + third + " " + fourth);
+        int set1 = first % 10;
+        int set2 = second % 10;
+        int set3 = third % 10;
+        int set4 = fourth % 10;
+        System.out.println(set1+ " " + set2  + " " + set3 + " " + set4);
+
+
+        System.out.println(set3 + " " + set4 + " " + set1 + " " + set2
+        );
+
+
+
+
+    }
+}
